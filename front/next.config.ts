b51fb6bb89d4ac1a9ed2b0edda159c1160fc58ca
LeeGamza <135112596+LeeGamza@users.noreply.github.com>
2024-12-19 16:04:50 +0900

    TRIP-3 <Docker 환경설정-Lee-2>

--- front/next.config.ts
+++ front/next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: 'export', // 정적 사이트로 내보내기 설정
 };
 
 export default nextConfig;
