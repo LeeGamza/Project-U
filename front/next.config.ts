950c52b0b6d586fe25a7a4cca0cf27823e594f2c
wnswo8628 <gbjwein1234@naver.com>
2024-12-20 20:32:53 +0900

    develop 수정

--- front/next.config.ts
+++
@@ -1,7 +0,0 @@
-import { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-    output: 'export', // 정적 사이트로 내보내기 설정
-};
-
-export default nextConfig;
--- front/src/app/testPage/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function TestPage() {
-    return (
-        <div>
-            <h1>Hello, this is Test Page!</h1>
-            <p>Welcome to the /testPage route.</p>
-        </div>
-    );
-}
