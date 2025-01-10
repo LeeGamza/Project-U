c7862adc930f5d4b855a6c708ba6e50178ed3c4f
LeeGamza <135112596+LeeGamza@users.noreply.github.com>
2025-01-10 14:16:42 +0900

    인트로페이지 작업-1

--- nginx/src/app/intro/page.tsx
+++ nginx/src/app/intro/page.tsx
@@ -16,20 +16,50 @@ export default function IntroPage() {
                     className={styles.logoImage}
                     />
                 <div className={styles.logoText}>TRIPSTORY</div>
-                <div className={styles.login}>로그인</div>
-                <div className={styles.register}>
-                    무료로 <span className={styles.boldText}>TRIPSTORY</span> 사용하기
+
+                <div className={styles.login}>
+
+                    <div className={styles.loginImage}>
+                        <Image
+                            src="/LogIn_Intro.png"
+                            alt="login_icon"
+                            width={20}
+                            height={20}
+                            className={styles.logoImage}
+                        />
+                    </div>
+
+                    <div className={styles.loginText}>
+                        로그인
+                    </div>
                 </div>
             </div>
 
             <div className={styles.down}>
-                <Image
-                    src="/World.png"
-                    alt="World"
-                    width={600}
-                    height={600}
-                    className={styles.worldImage}
-                />
+
+                <div className={styles.downText}>
+                    <p className={styles.indentedText}>대한민국 지역들을 <br /> 여행하고 기록해보세요.</p>
+                </div>
+
+                <div className={styles.kWorld}>
+                    <Image
+                        src="/KoreaWorld_Intro.png"
+                        alt="World"
+                        width={800}
+                        height={800}
+                        className={styles.worldImage}
+                    />
+                </div>
+
+                <div className={styles.sKImage}>
+                    <Image
+                        src="/SouthKorea_Intro.png"
+                        alt="SouthK"
+                        width={300}
+                        height={300}
+                        className={styles.southKImage}
+                    />
+                </div>
             </div>
         </div>
     );
